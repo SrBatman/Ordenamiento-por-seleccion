3465dc9c3588bb8c50b2d9ee3d90755543e5ec3a
SrBatman <71794521+SrBatman@users.noreply.github.com>
2024-05-30 10:29:47 -0600

    Create Casilla.java

---
+++ Casilla.java
@@ -0,0 +1,12 @@
+public class Casilla {
+    String ficha;
+
+    public String getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(String ficha) {
+        this.ficha = ficha;
+    }
+    
+}
